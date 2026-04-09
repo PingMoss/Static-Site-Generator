a7e963a0d3c36acac30b59b1b421eea713f6bb6c
PingMoss <connorraab10@gmail.com>
2026-04-08 21:36:44 -0400

    create src directory

---
+++ src/textnode.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD_TEXT = "bold text"
+    ITALIC_TEXT = "italic text"
+    CODE_TEXT = "code text"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        check_text = self.text == other.text
+        check_text_type = self.text_type.value == other.text_type.value
+        check_url = self.url == other.url
+
+        return check_text and check_text_type and check_url
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
