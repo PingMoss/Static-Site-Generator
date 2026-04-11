8902ae108505bda69e8d952e4a125642df444eaf
PingMoss <connorraab10@gmail.com>
2026-04-11 19:53:02 -0400

    Added tests for LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -30,6 +30,39 @@ class TestHTMLNode(unittest.TestCase):
     def test_repr_some(self):
         node = HTMLNode(value="This is a value")
         check = "Tag: None, Value: This is a value, Children: None, Props: None"
+        self.assertEqual(check, repr(node))
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_tagless(self):
+        node = LeafNode(tag=None, value="This is bold text.")
+        self.assertEqual(node.to_html(), "This is bold text.")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "google.com", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), "<a>google.com</a>")
+
+    def test_leaf_to_html_valueless(self):
+        node = LeafNode("a", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_repr_all(self):
+        node = LeafNode(
+            "a",
+            "This is content (tm)",
+            {"href": "https://www.google.com"},
+        )
+        check = "Tag: a, Value: This is content (tm), Props: {'href': 'https://www.google.com'}"
+        self.assertEqual(check, repr(node))
+
+    def test_repr_some(self):
+        node = LeafNode("p", "This is a value")
+        check = "Tag: p, Value: This is a value, Props: None"
         self.assertEqual(check, repr(node))
 
 
