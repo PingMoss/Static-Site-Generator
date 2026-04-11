dcad31477893bb04f95e941452f4570261a22ab1
PingMoss <connorraab10@gmail.com>
2026-04-11 19:52:31 -0400

    Added LeafNode class. Need to finish ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,30 @@ class HTMLNode:
 
     def __repr__(self):
         return f"Tag: {self.tag}, Value: {self.value}, Children: {self.children}, Props: {self.props}"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("Missing value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"Tag: {self.tag}, Value: {self.value}, Props: {self.props}"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Missing tag")
+        if self.children is None:
+            raise ValueError("Missing children")
+        # implement recursive function to return html of parent and children
