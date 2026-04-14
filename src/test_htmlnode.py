698e4d297bd7809581af90b86e92f0ca4964930c
PingMoss <connorraab10@gmail.com>
2026-04-13 21:07:47 -0400

    Added ParentNode tests and ammended previous tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -13,9 +13,9 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(check, node.props_to_html())
 
     def test_props_to_html_none(self):
-        # props is none by default
+        # props is "" by default
         node = HTMLNode()
-        self.assertEqual(None, node.props_to_html())
+        self.assertEqual("", node.props_to_html())
 
     def test_repr_all(self):
         node = HTMLNode(
@@ -44,11 +44,13 @@ class TestLeafNode(unittest.TestCase):
 
     def test_leaf_to_html_a(self):
         node = LeafNode("a", "google.com", {"href": "https://www.google.com"})
-        self.assertEqual(node.to_html(), "<a>google.com</a>")
+        self.assertEqual(
+            node.to_html(), '<a href="https://www.google.com">google.com</a>'
+        )
 
     def test_leaf_to_html_valueless(self):
         node = LeafNode("a", None)
-        with self.assertRaises(ValueError):
+        with self.assertRaisesRegex(ValueError, "Missing value"):
             node.to_html()
 
     def test_repr_all(self):
@@ -64,6 +66,75 @@ class TestLeafNode(unittest.TestCase):
         node = LeafNode("p", "This is a value")
         check = "Tag: p, Value: This is a value, Props: None"
         self.assertEqual(check, repr(node))
+
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_multiple_children(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
+        )
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_with_multiple_grandchildren(self):
+        """
+        parent - child_a - grandchild_a - great_grandchld_a
+                                        - great_grandchld_a_2
+               - child_b - grandchild_b
+               - child_c
+        """
+        great_grandchld_a = LeafNode("agg", "grandchild A", {"href": "www.example.com"})
+        great_grandchld_a_2 = LeafNode("agg2", "grandchild A 2")
+        grandchild_a = ParentNode("ag", [great_grandchld_a, great_grandchld_a_2])
+        child_a = ParentNode("ac", [grandchild_a])
+
+        grandchild_b = LeafNode("bg", "grandchild B")
+        child_b = ParentNode("bc", [grandchild_b], {"href": "www.example.com"})
+
+        child_c = LeafNode("c", "child C")
+
+        parent = ParentNode("p", [child_a, child_b, child_c])
+
+        check = '<p><ac><ag><agg href="www.example.com">grandchild A</agg><agg2>grandchild A 2</agg2></ag></ac><bc href="www.example.com"><bg>grandchild B</bg></bc><c>child C</c></p>'
+
+        self.assertEqual(parent.to_html(), check)
+
+    def test_to_html_tag_ValueError(self):
+        node = ParentNode(None, [LeafNode("p", "text")])
+        with self.assertRaisesRegex(ValueError, "Missing tag"):
+            node.to_html()
+
+    def test_to_html_children_ValueError(self):
+        node = ParentNode("div", None)
+        with self.assertRaisesRegex(ValueError, "Missing children"):
+            node.to_html()
+
+    def test_to_html_child_ValueError(self):
+        node = ParentNode("div", [LeafNode("p", "text"), LeafNode("p", None)])
+        with self.assertRaises(ValueError):
+            node.to_html()
 
 
 if __name__ == "__main__":
